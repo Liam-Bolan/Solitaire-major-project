e91c43c26479e96c62341883b79144019f64917e
Liam Bolan <AQ231688@aquinas.ac.uk>
2024-06-21 14:27:55 +0100

    Add project files.

---
+++ Solitaire major project/SolitaireGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solitaire_major_project
+{
+    public partial class SolitaireGame : Form
+    {
+        public SolitaireGame()
+        {
+            InitializeComponent();
+        }
+
+        private void SolitaireGame_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
